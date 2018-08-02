60b3c7dce902f4d7299ce4e1547845b02b1bd8b5
Drew Whitfield <apwhitfield13@gmail.com>
2018-08-02 23:33:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // write your code below!
 var name = "Susan"
-var height = "74"
+var height = "38"
 var message = `${name} is expected to be less than ${height} inches tall`
 
 module.exports = {name, height, message}
